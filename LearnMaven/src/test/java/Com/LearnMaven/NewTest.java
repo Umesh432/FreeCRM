44153b36832e870d8360616087a2dbab69edb7b5
umesh.s <umesh.s@com-umesh.dataone.com>
2018-12-08 00:40:16 +0530

    My First

---
+++ LearnMaven/src/test/java/Com/LearnMaven/NewTest.java
@@ -0,0 +1,21 @@
+package Com.LearnMaven;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void RUN1() {
+	  
+	  System.out.println("using testng test 1 class name NewTest");
+  }
+  @Test
+  public void RUN12() {
+	  
+	  System.out.println("using testng test 2 class name NewTest");
+  }
+  @Test
+  public void RUN3() {
+	  
+	  System.out.println("using testng test 3 class name NewTest");
+  }
+}
